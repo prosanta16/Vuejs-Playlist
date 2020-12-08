f70f1b0c3267cdafc745d6678439daa78e857cce
Prosanta Paul <prosantapaulbsmrstu@gmail.com>
2020-12-08 20:51:17 +0600

    tthat i s first

---
+++ src/components/Footer.vue
@@ -0,0 +1,41 @@
+<template>
+<footer>
+  <p>{{copyright}}{{title}}</p>
+</footer>
+
+</template>
+
+<script>
+import {bus} from '../main';
+export default {
+  props:{
+    title:{
+      type:String
+    }
+  },
+
+  data(){
+    return {
+       copyright:'Copy Right 2017 '
+    }
+  },
+  created(){
+    bus.$on('changeTitle',(data)=>{
+      this.title=data;
+    })
+  }
+  }
+
+</script>
+
+<style scoped>
+footer{
+  background: #222;
+  padding: 6px;
+}
+p{
+  color: lightgreen;
+  text-align: center;
+}
+
+</style>
